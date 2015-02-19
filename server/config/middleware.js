06dc637f475fe22409336838bf1bbe9b8db5e9dc
kchia <kchia87@gmail.com>
2015-02-18 21:56:09 -0800

    FIX fixes server file path to static assets

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -12,7 +12,7 @@ module.exports = function(app, express) {
   app.use(morgan('dev'));
   app.use(bodyParser.urlencoded({extended: true}));
   app.use(bodyParser.json());
-  app.use(express.static(__dirname + '/../client'));
+  app.use(express.static(__dirname + './../../dist'));
   // TODO: load routers based on request urls
   // Example: app.use('/api/users', userRouter);
   // use user router for all user request
